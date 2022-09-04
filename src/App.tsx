9b9715183f9547efa333b3b3acd30e0a2a14a8af
Computeneer <computeneer@gmail.com>
2022-09-04 14:13:30 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
 import "./styles/global.sass";
-import "styles/common.scss";
 
 const App: React.FC = () => {
   return <>App</>;
